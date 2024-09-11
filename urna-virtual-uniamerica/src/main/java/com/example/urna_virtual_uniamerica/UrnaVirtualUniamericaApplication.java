a8b0047eae7b912da6176f3ea87a7e3d27c67ba2
Matielo J Geronimo <matielojg@gmail.com>
2024-09-11 19:49:50 -0300

    Commit Inicial Urna Eletronica

---
+++ urna-virtual-uniamerica/src/main/java/com/example/urna_virtual_uniamerica/UrnaVirtualUniamericaApplication.java
@@ -0,0 +1,13 @@
+package com.example.urna_virtual_uniamerica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrnaVirtualUniamericaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrnaVirtualUniamericaApplication.class, args);
+	}
+
+}
---
+++ urna-virtual-uniamerica/src/test/java/com/example/urna_virtual_uniamerica/UrnaVirtualUniamericaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urna_virtual_uniamerica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrnaVirtualUniamericaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
